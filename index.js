b957f1ef4fabaac4e3299554bc871abbcb8e2681
Rohitap132 <136168201+Rohitap132@users.noreply.github.com>
2024-04-10 21:52:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-/**
- * @format
- */
+import React from 'react';
+import { View } from 'react-native';
+import Header from './Header';
+import MobileList from './MobileList';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+export default function App() {
+  return (
+    <View>
+      <Header />
+      <MobileList />
+    </View>
+  );
+}
